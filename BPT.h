77e4bd5d4885f30ebdb559132e57132cc0a8f10f
gyhyz917 <zhuyihan0826@outlook.com>
2025-04-13 09:28:20 +0800

    Add files initialization in BPT.h

--- BPT.h
+++ BPT.h
@@ -8,8 +8,8 @@
 template<class KEY,class OTHER,int M = 100,int L = 100>
 class BPT {
 private:
-    std::fstream indexTree;//索引块
-    std::fstream leaf;//数据块
+    std::fstream indexTree;//索引块  前2个int大小的块存nextIndexPos和 root.pos 先后顺序就是这个
+    std::fstream leaf;//数据块 前2个int大小的块存nextLeafPos和 FirstLeaf.Pos 先后顺序就是这个
     std::string indexTree_name;
     std::string leaf_name;
 
@@ -20,6 +20,8 @@ private:
         int pos;//当前结点在disk上的位置
         KEY Key[M - 1];
         int ChildPointer[M];//磁盘位置
+
+        IndexNode(bool whether,int k = 0,int c = 0,int p = 1):is_leaf(whether),keyNum(k),childNum(c),pos(p){}
     };
 
     struct LeafNode {
@@ -28,9 +30,52 @@ private:
         int num;//存储键值对数量
         std::pair<KEY,OTHER> Info[L];//键值对信息
         int next;//下一个LeafNode位置
+
+        LeafNode(bool whether = true,int p = 1,int n = 0,int nxt):is_leaf(whether),pos(p),num(n),next(nxt) {}
     };
 
     IndexNode root;
     LeafNode Data;
+    long long int totalNum = 0;
+    int nextIndexNodePos;
+    int nextLeafNodePos;
+
+    void writeInfoIndex(IndexNode &written_index,int position) {
+        indexTree.seekp(2 * sizeof(int) + position * sizeof(IndexNode));
+        indexTree.write(reinterpret_cast<char*>(&written_index),sizeof(IndexNode));
+    }
+
+    void writeInfoLeaf(LeafNode &written_leaf,int position) {
+        leaf.seekp(2 * sizeof(int) + position * sizeof(LeafNode));
+        leaf.write(reinterpret_cast<char*>(&written_leaf),sizeof(LeafNode));
+    }
+    void initialize() {
+        root.is_leaf = true;
+        root.keyNum = 0;
+        root.childNum = 1;
+        root.pos = 1;
+        root.ChildPointer[0] = 1;
+        totalNum = 0;
+        nextIndexNodePos = 1;
+        nextLeafNodePos = 1;
+        LeafNode FirstLeaf(true,1,0,0);
+        writeInfoLeaf(FirstLeaf,1);//TODO Wait further check.
+    }
+public:
+    BPT(const std::string &s1,const std::string &s2) {
+        indexTree_name = s1;
+        leaf_name = s2;
+        indexTree.open(indexTree_name,std::ios::binary | std::ios::in | std::ios::out);
+        leaf.open(leaf_name,std::ios::binary | std::ios::in | std::ios::out);
+        if(!indexTree && !leaf) {
+            indexTree.open(indexTree_name,std::ios::binary | std::ios::out);
+            leaf.open(leaf_name,std::ios::binary | std::ios::out);
+            initialize();
+            indexTree.close();
+            leaf.close();
+            indexTree.open(indexTree_name,std::ios::binary | std::ios::in | std::ios::out);
+            leaf.open(leaf_name,std::ios::binary | std::ios::in | std::ios::out);
+        }
+    }
 };
 #endif //BPT_H
--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include "BPT.h"
 int main() {
     int n;
     std::cin >> n;
-    std::string op;
-    int index,value;
+    std::string op,index;
+    BPT<const char*,int,100,100> BPlusTree("IndexFile","LeafFile");
+    int value;
     for(int i = 1;i <= n;i ++) {
         std::cin >> op;
-
         if(op == "insert") {
             std::cin >> index >> value;
+            const char* tranfer_index = index.c_str();
         }else if(op == "delete") {
             std::cin >> index >> value;
+            const char* tranfer_index = index.c_str();
         }else if(op == "find") {
             std::cin >> index;
+            const char* tranfer_index = index.c_str();
         }
     }
 }
