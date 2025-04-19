be3b23e4efb5efe3318a7fdcafccfa2f509ba0d7
Yihan Zhu <zhuyihan0826@outlook.com>
2025-04-19 10:51:32 +0800

    Git Failed

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 #include <string>
 #include <cstring>
 #include "BPT.h"
+unsigned long long customHash(const char* str) {
+    unsigned long long hash = 0;
+    unsigned long long prime = 31; // 选择一个小的素数作为乘积因子
+    for (size_t i = 0; i < strlen(str); ++i) {
+        hash = hash * prime + str[i];
+    }
+    return hash;
+}
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
@@ -10,22 +18,25 @@ int main() {
     int n;
     std::cin >> n;
     std::string op,index;
-    BPT<const char*,int,100,100> BPlusTree("IndexFile.txt","LeafFile.txt");
+    BPT<unsigned long long int,int,100,100> BPlusTree("IndexFile","LeafFile");
     int value;
     for(int i = 1;i <= n;i ++) {
         std::cin >> op;
         if(op == "insert") {
             std::cin >> index >> value;
             const char* tranfer_index = index.c_str();
-            BPlusTree.insert(tranfer_index,value);
+            unsigned long long int HashChar = customHash(tranfer_index);
+            BPlusTree.insert(HashChar,value);
         }else if(op == "delete") {
             std::cin >> index >> value;
             const char* tranfer_index = index.c_str();
-            BPlusTree.erase(tranfer_index,value);
+            unsigned long long int HashChar = customHash(tranfer_index);
+            BPlusTree.erase(HashChar,value);
         }else if(op == "find") {
             std::cin >> index;
             const char* tranfer_index = index.c_str();
-            sjtu::vector<int> tmp = BPlusTree.find(tranfer_index);
+            unsigned long long int HashChar = customHash(tranfer_index);
+            sjtu::vector<int> tmp = BPlusTree.find(HashChar);
             if(tmp.empty()) {
                 std::cout << "null" << '\n';
             }else {
